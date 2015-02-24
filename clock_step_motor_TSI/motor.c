57831d2b5141be5411eeb3a285db5d5a62e84119
Eugene <zenjka.1@gmail.com>
2015-02-25 01:06:01 +0200

    Update motor.c

--- clock_step_motor_TSI/motor.c
+++ clock_step_motor_TSI/motor.c
@@ -9,15 +9,40 @@
 
 #define MOTOR_PORT PORTB
 #define PIN_DIR 0
-#define PIN_STEP 1
+#define PIN_STEP 3
 
-void motor_step(char clockwise, unsigned char number )
+void motor_step(char direction, unsigned char number )
 {
-	MOTOR_PORT |= ( (1 & clockwise) << PIN_DIR );		// choose direction of rotation
+	MOTOR_PORT |= ( (1 & direction) << PIN_DIR );		// choose direction of rotation
 	
-	for(unsigned n = 0; n < number * 200; n++)	// steps
+	for(unsigned n = 0; n < number * 20; n++)	// steps
 	{
 		MOTOR_PORT ^= ( 1 << PIN_STEP );	
 		_delay_us(500);
 	}
+}
+
+void motor_rotate(unsigned char direction)
+{
+	if(direction == 0)
+	{
+		TCCR2	&= ~(1<<COM20); // OC2 NORMAL operation mode
+		TCCR1B	&= ~((1<<CS10)|(1<<CS10));	// Disable timer
+		TCNT1 = 0;
+	}
+	else
+	{
+		TCCR1B	|= (1<<CS11)|(1<<CS10);	//  No prescaling,  enable timer
+		TCCR2	|= (1<<COM20); // OC2 PWM
+		TIFR	|= (1<<OCF1A);	//clear interrupt flag
+		MOTOR_PORT &= ~((1 & direction) << PIN_DIR );
+	}
+}
+
+void motor_set_time(unsigned char new_time)
+{
+	unsigned char hours = new_time / 100;
+	unsigned char minutes = new_time % 100;
+	
+	motor_step(1, hours * 60 + minutes );
 }
